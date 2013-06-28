4390848b37f43a68606c7290d791c7216bf0c56e
唐富伟 <tianskyluj@sina.com>
2013-06-28 17:44:06 +0800

    初始化为管理员

--- SQLFiles/初始化/初始化.sql
+++ SQLFiles/初始化/初始化.sql
@@ -1,7 +1,7 @@
 
 -- 插入超级用户	用户名：lan -- 密码：12346 (OrwmNwHIjJI=)
 truncate table SystemUser
-insert into SystemUser(userName,passWord,name)values('lan','2W9E012dgLzwivCZHVZUZQ==','唐富伟')
+insert into SystemUser(userName,passWord,name,ifAdmin)values('lan','2W9E012dgLzwivCZHVZUZQ==','唐富伟',1)
 
 truncate table GlobalSetting
 -- 初始化全局变量 
