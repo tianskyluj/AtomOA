3544d884f2b47292bffd789ecd960f22ff20fe79
唐富伟 <tianskyluj@sina.com>
2013-07-01 18:32:22 +0800

    显示部门列表

--- Program/Web/BasicSetting/Department.aspx.cs
+++ Program/Web/BasicSetting/Department.aspx.cs
@@ -5,10 +5,17 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Web_BasicSetting_Department : System.Web.UI.Page
+public partial class Web_BasicSetting_Department : BasePage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        bindGridView();
+    }
 
+    protected void bindGridView()
+    {
+        ATOM.BLL.Department department_BLL = new ATOM.BLL.Department();
+        this.dataRow.DataSource = department_BLL.GetAllList().Tables[0];
+        this.dataRow.DataBind();
     }
 }
